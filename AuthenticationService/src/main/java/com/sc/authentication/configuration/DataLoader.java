3e2f1b6be77ee14994b853cebbb20d921e4324b4
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-01-29 18:02:47 +0530

    cleanup

--- AuthenticationService/src/main/java/com/sc/authentication/configuration/DataLoader.java
+++ AuthenticationService/src/main/java/com/sc/authentication/configuration/DataLoader.java
@@ -2,25 +2,27 @@ package com.sc.authentication.configuration;
 
 import com.sc.authentication.model.Roles;
 import com.sc.authentication.repository.RoleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class DataLoader {
 
     @Bean
-    CommandLineRunner loadRolesInDatabase(RoleRepository roleRepository) {
+    public CommandLineRunner loadRolesInDatabase(RoleRepository roleRepository) {
         return args -> {
-            if (roleRepository.existsByRoleName("ROLE_ADMIN")) {
-                roleRepository.save(Roles.builder().roleName("ROLE_ADMIN")
-                    .description("Administrator access").build()
-                );
+            if (!roleRepository.existsByRoleName("ROLE_ADMIN")) {
+                Roles roleAdmin = roleRepository.save(Roles.builder().roleName("ROLE_ADMIN")
+                    .description("Administrator access").build());
+                log.info("ADMIN ROLE added::{}", roleAdmin);
             }
-            if (roleRepository.existsByRoleName("ROLE_USER")) {
-                roleRepository.save(Roles.builder().roleName("ROLE_USER")
-                    .description("Administrator access").build()
-                );
+            if (!roleRepository.existsByRoleName("ROLE_USER")) {
+                Roles roleUser = roleRepository.save(Roles.builder().roleName("ROLE_USER")
+                    .description("Administrator access").build());
+                log.info("USER ROLE added::{}", roleUser);
             }
         };
     }
